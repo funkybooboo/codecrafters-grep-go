fd1d67d568bd139feb51c8a09e1d525bb63dc816
Nate Stott <nate.stott@pm.me>
2025-08-03 12:58:10 -0400

    [any message]

--- app/main.go
+++ app/main.go
@@ -14,7 +14,10 @@ var _ = bytes.ContainsAny
 // Usage: echo <input_text> | your_program.sh -E <pattern>
 func main() {
 	if len(os.Args) < 3 || os.Args[1] != "-E" {
-		fmt.Fprintf(os.Stderr, "usage: mygrep -E <pattern>\n")
+		_, err := fmt.Fprintf(os.Stderr, "usage: mygrep -E <pattern>\n")
+		if err != nil {
+			return
+		}
 		os.Exit(2) // 1 means no lines were selected, >1 means error
 	}
 
@@ -22,13 +25,19 @@ func main() {
 
 	line, err := io.ReadAll(os.Stdin) // assume we're only dealing with a single line
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: read input text: %v\n", err)
+		_, err := fmt.Fprintf(os.Stderr, "error: read input text: %v\n", err)
+		if err != nil {
+			return
+		}
 		os.Exit(2)
 	}
 
 	ok, err := matchLine(line, pattern)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		_, err := fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		if err != nil {
+			return
+		}
 		os.Exit(2)
 	}
 
@@ -47,10 +56,12 @@ func matchLine(line []byte, pattern string) (bool, error) {
 	var ok bool
 
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
+	_, err := fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
+	if err != nil {
+		return false, err
+	}
 
-	// Uncomment this to pass the first stage
-	// ok = bytes.ContainsAny(line, pattern)
+	ok = bytes.ContainsAny(line, pattern)
 
 	return ok, nil
 }
